029489e5eb724b94870f3b0365ade83b60fd4c10
burno <bruno.wieczorek@gmail.com>
2021-01-10 23:38:56 +0100

    Replace constraint with Box

--- app/src/main/java/pl/burno/selectionlauncher/ui/components/main/progress/FullScreenProgressBar.kt
+++ app/src/main/java/pl/burno/selectionlauncher/ui/components/main/progress/FullScreenProgressBar.kt
@@ -1,25 +1,16 @@
 package pl.burno.selectionlauncher.ui.components.main.progress
 
-import androidx.compose.foundation.layout.ConstraintLayout
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showBackground = true)
 @Composable
 fun FullScreenProgressBar() {
-    ConstraintLayout(Modifier.fillMaxWidth().fillMaxHeight()) {
-        val progressIndicator = createRef()
-        CircularProgressIndicator(
-            Modifier.constrainAs(progressIndicator) {
-                top.linkTo(parent.top)
-                bottom.linkTo(parent.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-        )
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
     }
 }
